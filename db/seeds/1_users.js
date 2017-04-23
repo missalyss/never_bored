899f76a236f552ddada0a1269dc69c4339afcac8
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-04-22 18:46:38 -0700

    Updated users and activities seeds

--- db/seeds/1_users.js
+++ db/seeds/1_users.js
@@ -3,9 +3,9 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return knex('users').insert([
-        {id: 1, username: 'rowValue1', email: 'email@email.com', hashed_pw: '', avatar_url: 'http://fillmurray.com/80/80'},
-        {id: 2, username: 'rowValue2', email: 'email@email.com', hashed_pw: '', avatar_url: 'http://fillmurray.com/80/80'},
-        {id: 3, username: 'rowValue3', email: 'email@email.com', hashed_pw: '', avatar_url: 'http://fillmurray.com/80/80'}
+        {id: 1, username: 'missalyss', email: 'missalyss@email.com', hashed_pw: '', avatar_url: 'http://fillmurray.com/80/80'},
+        {id: 2, username: 'andromeda111', email: 'andromeda111@email.com', hashed_pw: '', avatar_url: 'http://fillmurray.com/80/80'},
+        {id: 3, username: 'amaliaclaire', email: 'amaliaclaire@email.com', hashed_pw: '', avatar_url: 'http://fillmurray.com/80/80'}
       ])
     }).then(function () {
         return knex.raw(
--- db/seeds/2_activites.js
+++ db/seeds/2_activites.js
@@ -3,9 +3,24 @@ exports.seed = function(knex, Promise) {
   return knex('activities').del()
     .then(function () {
       return knex('activities').insert([
-        {id: 1, title: 'rowValue1', description: 'description', cost: 2, energy: 2, time: 2, location: 2, party: 2, adult: false, img_url: 'http://fillmurray.com/100/100', creator_id: 2},
-        {id: 2, title: 'rowValue2', description: 'description', cost: 2, energy: 2, time: 2, location: 2, party: 2, adult: false, img_url: 'http://fillmurray.com/100/100', creator_id: 2},
-        {id: 3, title: 'rowValue3', description: 'description', cost: 2, energy: 2, time: 2, location: 2, party: 2, adult: false, img_url: 'http://fillmurray.com/100/100', creator_id: 2}
+        {
+          id: 1, title: 'Bad Movies Series', description: 'Who doesn\'t love to make fun of terrible movies with their friends? Try this series of bad movies, either as a marathon or a weekly event. 1. The Room 2. Troll 2 3. Birdemic 4. Titanic 2. Time commitment refers to each film, not the series.', cost: 1, energy: 1, time: 2, location: 2, party: 3, adult: true, img_url: 'http://fillmurray.com/100/100', creator_id: 1
+        },
+        {
+          id: 2, title: 'rowValue2', description: 'description', cost: 2, energy: 2, time: 2, location: 2, party: 2, adult: false, img_url: 'http://fillmurray.com/100/100', creator_id: 2
+        },
+        {
+          id: 3, title: 'rowValue3', description: 'description', cost: 2, energy: 2, time: 2, location: 2, party: 2, adult: false, img_url: 'http://fillmurray.com/100/100', creator_id: 2
+        },
+        {
+          id: 4, title: 'rowValue3', description: 'description', cost: 2, energy: 2, time: 2, location: 2, party: 2, adult: false, img_url: 'http://fillmurray.com/100/100', creator_id: 2
+        },
+        {
+          id: 5, title: 'rowValue3', description: 'description', cost: 2, energy: 2, time: 2, location: 2, party: 2, adult: false, img_url: 'http://fillmurray.com/100/100', creator_id: 2
+        },
+         {
+           id: 6, title: 'rowValue3', description: 'description', cost: 2, energy: 2, time: 2, location: 2, party: 2, adult: false, img_url: 'http://fillmurray.com/100/100', creator_id: 2
+         }
       ])
     }).then(function () {
         return knex.raw(
