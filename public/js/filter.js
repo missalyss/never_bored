c793b73558690a4e5e537141202aab848d5b7b9a
John <jmseymour111@gmail.com>
2017-04-24 14:05:57 -0700

    working on search filtering, nothing new really

---
+++ public/js/filter.js
@@ -0,0 +1,20 @@
+
+function testGet () {
+  console.log(allActivities);
+  return
+}
+
+$('li').click(testGet)
+
+
+// $.ajax({
+//     url: '',
+//     method: 'GET'
+// }).then(function(result) {
+//     let quoteTxt = result.quoteText;
+//     let quoteAuth = result.quoteAuthor;
+//     $('.insp-quote-block').append(`<h3>"${quoteTxt}"</h3>`)
+//     $('.insp-quote-block').append(`<h4>–${quoteAuth}</h4>`)
+// }).catch(function(error) {
+//     console.log('Error: ', error);
+// })
