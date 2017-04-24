9bf66d244c67b201669ae46092e13a715862e193
John <jmseymour111@gmail.com>
2017-04-24 14:05:57 -0700

    working on search filtering, nothing new really

--- routes/search.js
+++ routes/search.js
@@ -5,12 +5,8 @@ var knex = require('../db/connection')
 /* GET home page. */
 router.get('/', (req, res, next) => {
   knex('activities').then((allActivities) => {
-    res.render('index', { allActivities })
+    res.render('search', { allActivities })
   })
-})
-
-router.get('/login', (req, res, next) => {
-  res.render('login')
 })
 
 module.exports = router
