61b9fa06cef3716bd187277af438e37668e4a9d9
John <jmseymour111@gmail.com>
2017-04-24 15:25:10 -0700

    working on search filtering, updated Layout

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,20 +1,21 @@
 
 function testGet () {
-  console.log(allActivities);
-  return
+  var incomingData = ajaxCall()
+  console.log('//////////////////////');
+  console.log(incomingData);
 }
 
-$('li').click(testGet)
+$('#cost').click(testGet)
 
-
-// $.ajax({
-//     url: '',
-//     method: 'GET'
-// }).then(function(result) {
-//     let quoteTxt = result.quoteText;
-//     let quoteAuth = result.quoteAuthor;
-//     $('.insp-quote-block').append(`<h3>"${quoteTxt}"</h3>`)
-//     $('.insp-quote-block').append(`<h4>–${quoteAuth}</h4>`)
-// }).catch(function(error) {
-//     console.log('Error: ', error);
-// })
+function ajaxCall() {
+  $.ajax({
+    url: 'http://localhost:3223/search/data',
+    method: 'GET'
+  }).then(function (result) {
+    console.log('ajax');
+    console.log(result);
+      return result
+  }).catch(function (error) {
+    console.log('Error: ', error)
+  })
+}
