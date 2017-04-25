9c1227d1d531b783644c0780a766787b534b3186
John <jmseymour111@gmail.com>
2017-04-25 14:55:05 -0700

    basic filtering working

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,29 +1,50 @@
-// var catValue
+var $filterCheckboxes = $('input[type="checkbox"]');
 
-//
-// $('#relaxing').click(testHideCategory)
-//
-// function testHideCategory () {
-  // let cTypes = $("selected")
-  // console.log('This works')
-  // var catName = $('.infotest').attr('data-category')
-  // console.log(catName)
-//   // if (catName === 'Relaxing') {
-//     $("div[data-category='Relaxing']").hide()
-//   // }
-// }
+$filterCheckboxes.on('change', function() {
 
+  var selectedFilters = {};
 
-$('.checkbox').click(function () {
-  var catValue = $(this).val()
-  console.log(catValue);
-  // var catValue = $('input[type="checkbox"]').val()
-  // console.log(catValue);
-  functionHide(catValue)
+  $filterCheckboxes.filter(':checked').each(function() {
 
+    if (!selectedFilters.hasOwnProperty(this.name)) {
+      selectedFilters[this.name] = [];
+    }
 
-})
+    selectedFilters[this.name].push(this.value);
 
-function functionHide (catValue) {
-    $(`div[data-category="${catValue}"]`).toggle()
-}
+  });
+  console.log(selectedFilters);
+  // create a collection containing all of the filterable elements
+  var $filteredResults = $('.activity');
+
+  // loop over the selected filter name -> (array) values pairs
+  $.each(selectedFilters, function(name, filterValues) {
+
+    // filter each .flower element
+    $filteredResults = $filteredResults.filter(function() {
+
+      var matched = false,
+        currentFilterValues = $(this).data('category').split(' ');
+
+      // loop over each category value in the current .flower's data-category
+      $.each(currentFilterValues, function(_, currentFilterValue) {
+        console.log(currentFilterValues, currentFilterValue, filterValues);
+        // if the current category exists in the selected filters array
+        // set matched to true, and stop looping. as we're ORing in each
+        // set of filters, we only need to match once
+
+        if ($.inArray(currentFilterValue, filterValues) != -1) {
+          matched = true;
+          return false;
+        }
+      });
+
+      // if matched is true the current .flower element is returned
+      return matched;
+
+    });
+  });
+
+  $('.activity').hide().filter($filteredResults).show();
+
+});
