449fe2af1110d3b03e6f0675dc82c72ab3cb05ae
John <jmseymour111@gmail.com>
2017-04-26 14:32:48 -0700

    Made Search page into the Index page

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,16 @@ var knex = require('../db/connection')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  knex('activities').then((allActivities) => {
-    knex('categories').then((allCategories)=>{
-
-
-      data = {allActivities, allCategories }
-      console.log(data)
-      res.render('index', data )
-    })
+  return knex.select('activities.title', 'activities.id', 'activities.cost', 'activities.energy', 'activities.time', 'activities.location', 'activities.party', 'activities.adult', 'activities.creator_id', 'activities.img_url', 'categories.name')
+  .from('activities')
+  .innerJoin('tags_join', 'tags_join.activity_id', 'activities.id')
+  .innerJoin('categories', 'tags_join.category_id', 'categories.id')
+  .orderBy('activities.id', 'asc')
+  .then(allActivities => {
+    console.log(allActivities)
+    res.render('index', { allActivities })
+  }).catch(err => {
+    next(err)
   })
 })
 
