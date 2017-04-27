ee4dc2de248bc66e27448828c386654e853be1bc
John <jmseymour111@gmail.com>
2017-04-26 21:10:40 -0700

    it works

--- routes/guide.js
+++ routes/guide.js
@@ -19,14 +19,24 @@ router.get('/', (req, res, next) => {
 })
 
 router.get('/data', (req, res, next) => {
-  console.log(req.query);
+  console.log(req.query)
+  var queryData = {
+    location: req.query.location,
+    cost: req.query.cost
+  }
   knex.select('activities.title', 'activities.id', 'activities.cost', 'activities.energy', 'activities.time', 'activities.location', 'activities.party', 'activities.adult', 'activities.creator_id', 'activities.img_url', 'categories.name')
   .from('activities')
   .innerJoin('tags_join', 'tags_join.activity_id', 'activities.id')
   .innerJoin('categories', 'tags_join.category_id', 'categories.id')
-  .then(allActivities => {
+  .where(queryData)
+  .then(guideActivity => {
+    var arrayLength = guideActivity.length
     //
-    res.render('guide', { allActivities })
+    if (guideActivity.length > 1) {
+      var randomActivity = guideActivity[Math.floor(Math.random() * arrayLength)]
+      console.log()
+    }
+    res.redirect(`/activities/${randomActivity.id}`)
   })
 })
 
