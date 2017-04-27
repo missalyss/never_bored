e9288a6550d749cc9082fd42de6f86270cec5574
John <jmseymour111@gmail.com>
2017-04-27 10:15:24 -0700

    guide me pretty much done, needs error catch

--- public/js/guidejquery.js
+++ public/js/guidejquery.js
@@ -1,28 +1,31 @@
+$('input').click(function() {
+  $('.guide-buttons').show()
+})
 
 $('#g-next1').click(function (e) {
   e.preventDefault()
-  $('.guide-div-1').hide()
-  $('.guide-div-2').show()
+  $('.guide-buttons').hide()
+  $('#guide-div-1').hide()
+  $('#guide-div-2').show()
 })
 
-// $('#g-form1').submit(function (e) {
-//   e.preventDefault()
-//   var data = $('#g-form1').serializeArray().reduce(function(obj, item) {
-//     obj[item.name] = item.value
-//     return obj
-// }, {})
-//   console.log(data)
-  // Ajax call to database
-  // $.ajax({
-  //       url: `http://localhost:3223/guide/data`,
-  //       method: 'GET'
-  //   }).then(function(result) {
-  //
-  //   }).catch(function(error) {
-  //       console.log('Error: ', error);
-  //   })
+$('#g-next2').click(function (e) {
+  e.preventDefault()
+  $('.guide-buttons').hide()
+  $('#guide-div-2').hide()
+  $('#guide-div-3').show()
+})
 
+$('#g-next3').click(function (e) {
+  e.preventDefault()
+  $('.guide-buttons').hide()
+  $('#guide-div-3').hide()
+  $('#guide-div-4').show()
+})
 
-
-  // window.location.href = '/'
-// })
+$('#g-next4').click(function (e) {
+  e.preventDefault()
+  $('.guide-buttons').hide()
+  $('#guide-div-4').hide()
+  $('#guide-div-5').show()
+})
